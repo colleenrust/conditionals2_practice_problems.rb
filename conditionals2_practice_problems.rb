e1ebb6f9f9755af294a2e90517da0a7541f5c32e
Colleen <colleenrust524@EMAIL.com>
2023-03-22 14:15:15 -0500

    add #1 practice

--- conditionals2_practice_problems.rb
+++ conditionals2_practice_problems.rb
@@ -8,7 +8,9 @@ else
   p -1
 end
 # 2. Use a variable to store a number, then write a condition that prints -1 if the number is less than 10, prints 1 if the number is greater than 10, and prints 0 if the number is equal to 10.
+a = 7
 
+if a
 
 # 3. Use variables to store two numbers, then write a condition that prints 1 if the numbers are both less than 10, and prints 0 otherwise.
 
