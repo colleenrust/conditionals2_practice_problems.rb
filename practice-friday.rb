4c15413ba234464eefa256c45518385c9f7c66fd
Colleen <colleenrust524@EMAIL.com>
2023-03-24 13:33:31 -0500

    add comments of syracuse marketplaceproximity demo, add employee name, add method to find if employee is full time or part time

--- practice-friday.rb
+++ practice-friday.rb
@@ -1,19 +1,58 @@
+# require 'http'
+
+# response = HTTP.get("https://data.ny.gov/resource/xjya-f8ng.json")
+# markets = response.parse(:json)
+
+
+# #show all farmers markets
+# count = 0
+# close_to_cuse_markets = []
+
+# markets.each do |market|
+#   if market['phone'][0...3] == '315'
+#     count += 1
+#     close_to_cuse_markets << market
+#     p "I AM NEAR SYRACUSE"
+#   end
+# end
+# p count
+# pp close_to_cuse_markets
+    
+
 require 'http'
 
-response = HTTP.get("https://data.ny.gov/resource/xjya-f8ng.json")
-markets = response.parse(:json)
+ 
+full_or_part_time = ['P','F']
+response = HTTP.get("https://data.cityofchicago.org/resource/xzkq-xp2w.json")
 
+employees = response.parse(:json)
+full_time= [] 
 
-#show all farmers markets
-count = 0
-close_to_cuse_markets = []
+#show all the employees who work full time
 
-markets.each do |market|
-  if market['phone'][0...3] == '315'
-    count += 1
-    close_to_cuse_markets << market
-    p "I AM NEAR SYRACUSE"
-  end
+employees.each do |employee|
+
+  p employee['name'] 
+  p employee['full_or_part_time']
+
+  if employee['full_or_part_time'] == 'P'
+    p "Part time!"
+    elsif employee['full_or_part_time'] == 'F'
+      p "FULL TIME"
+  end 
 end
-p count
-pp close_to_cuse_markets
+p employees['full_or_part_time'].to_s
+#full employee or part-time
+# p employees[name]
+
+
+
+
+# if employee['full_or_part_time'] == 'F'
+#   full_or_part_time << full_time
+#   p "#{:employee['name']} is a Full Time: employee"
+# elsif employee['full_or_part_time'] == 'P'
+#   p "#{employee['name']} is a Part Time: employee"
+# else
+#   p "None of your business!!!!!"
+# end
